adeb86874ca47e924ff5f5df338aae890faaee9d
Cheryl Patrick <c.patrick@ucl.ac.uk>
2020-05-14 22:00:34 +0100

    Initial commit

---
+++ TrackCurvature.cpp
@@ -0,0 +1,145 @@
+#include "TrackCurvature.h"
+
+
+using namespace std;
+DPP_MODULE_REGISTRATION_IMPLEMENT(TrackCurvature,"TrackCurvature");
+TrackCurvature::TrackCurvature() : dpp::base_module()
+{
+  filename_output_="trackcurvature.root";
+}
+
+TrackCurvature::~TrackCurvature() {
+  if (is_initialized()) this->reset();
+}
+
+void TrackCurvature::initialize(const datatools::properties& myConfig,
+                                   datatools::service_manager& flServices,
+                                   dpp::module_handle_dict_type& /*moduleDict*/){
+
+  // Look for services
+  if (flServices.has("geometry")) {
+    const geomtools::geometry_service& GS = flServices.get<geomtools::geometry_service> ("geometry");
+    geometry_manager_ = &GS.get_geom_manager();
+    DT_THROW_IF(!geometry_manager_,
+                std::runtime_error,
+                "Null pointer to geometry manager return by geometry_service");
+  }
+  // Extract the filename_out key from the supplied config, if
+  // the key exists. datatools::properties throws an exception if
+  // the key isn't in the config, so catch this if thrown and don't do
+  // anything
+  try {
+    myConfig.fetch("filename_out",this->filename_output_);
+  } catch (std::logic_error& e) {
+  }
+
+  // Use the method of PTD2ROOT to create a root file with just the branches we need for the sensitivity analysis
+
+
+  hfile_ = new TFile(filename_output_.c_str(),"RECREATE","Basic track curve info");
+  hfile_->cd();
+  tree_ = new TTree("Curvature","Curvature");
+  tree_->SetDirectory(hfile_);
+  
+  // Reconstructed quantities
+  
+  // Standard cuts
+  
+  
+  // Some basic counts
+  tree_->Branch("reco.track_count",&trackdetails_.track_count_);
+  tree_->Branch("reco.electron_count",&trackdetails_.electron_count_);
+  tree_->Branch("reco.electron_charges",&trackdetails_.electron_charges_);
+  tree_->Branch("reco.track_curvatures",&trackdetails_.track_curvatures_);
+  tree_->Branch("reco.track_charges",&trackdetails_.track_charges_);
+  tree_->Branch("reco.electron_energies",&trackdetails_.electron_energies_);
+  tree_->Branch("reco.electron_track_lengths", &trackdetails_.electron_track_lengths_);
+  tree_->Branch("reco.electron_hit_counts",&trackdetails_.electron_hit_counts_);
+
+  this->_set_initialized(true);
+}
+//! [TrackCurvature::Process]
+dpp::base_module::process_status
+TrackCurvature::process(datatools::things& workItem) {
+  
+
+  uint electronCount=0;
+  int trackCount=0;
+  std::vector<snemo::datamodel::particle_track> electronCandidates;
+  std::vector<double> electronEnergies;
+  std::vector<int> electronCharges;
+  std::vector<double> electronTrackLengths;
+  std::vector<double> trackCurvatures;
+  std::vector<int> trackCharges;
+  std::vector<int> electronHitCounts;
+
+  
+// Number of particle tracks PTD databank
+  try
+  {
+   const snemo::datamodel::particle_track_data& trackData = workItem.get<snemo::datamodel::particle_track_data>("PTD");
+
+   if (trackData.hasParticles ())
+    {
+
+      const snemo::datamodel::ParticleHdlCollection particles = trackData.particles();
+      for (snemo::datamodel::ParticleHdlCollection::const_iterator   iParticle = particles.begin(); iParticle != particles.end(); ++iParticle) {
+        const snemo::datamodel::particle_track& track = iParticle->get();
+      }
+    }
+  }
+  catch (std::logic_error& e) {
+    std::cerr << "failed to grab PTD bank : " << e.what() << std::endl;
+    return dpp::base_module::PROCESS_INVALID;
+  } //end catch
+// Initialise variables that might not otherwise get set
+ // It does not restart the vector for each entry so we have to do that manually
+  ResetVars();
+
+
+  trackdetails_.electron_track_lengths_=electronTrackLengths;
+  trackdetails_.electron_hit_counts_=electronHitCounts;
+  trackdetails_.electron_charges_=electronCharges;
+  trackdetails_.track_charges_=trackCharges;
+  trackdetails_.track_curvatures_=trackCurvatures;
+  trackdetails_.electron_energies_=electronEnergies;
+  trackdetails_.track_count_=trackCount;
+  trackdetails_.electron_count_=electronCount;
+
+//
+
+
+  tree_->Fill();
+//
+//  // MUST return a status, see ref dpp::processing_status_flags_type
+ return dpp::base_module::PROCESS_OK;
+}
+
+void TrackCurvature::ResetVars()
+{
+  // clear out all the vectors or they persist between events
+    trackdetails_.electron_track_lengths_.clear();
+    trackdetails_.electron_charges_.clear();
+    trackdetails_.track_charges_.clear();
+    trackdetails_.electron_hit_counts_.clear();
+    trackdetails_.electron_energies_.clear();
+    trackdetails_.track_curvatures_.clear();
+    trackdetails_.track_count_=0;
+    trackdetails_.electron_count_=0;
+  
+}
+
+//! TrackCurvature::reset]
+void TrackCurvature::reset() {
+  hfile_->cd();
+  tree_->Write();
+  hfile_->Close(); //
+  std::cout << "In reset: finished conversion, file closed " << std::endl;
+
+  // clean up
+  delete hfile_;
+  filename_output_ = "trackcurvature.root";
+  this->_set_initialized(false);
+
+}
+
---
+++ TrackCurvature.h
@@ -0,0 +1,89 @@
+//! \file    TrackCurvature.h
+//! \brief   Get charge and curvature of reconstructed tracks
+//! \details Process a things object
+#ifndef TRACKCURVE_HH
+#define TRACKCURVE_HH
+// Standard Library
+// Third Party
+//#include <boost/foreach.hpp>
+#include "TFile.h"
+#include "TTree.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TVector3.h"
+
+// - Bayeux
+#include "bayeux/dpp/base_module.h"
+#include "bayeux/mctools/simulated_data.h"
+#include "bayeux/genbb_help/primary_particle.h"
+#include "bayeux/genbb_help/primary_event.h"
+#include "bayeux/datatools/service_manager.h"
+#include "bayeux/geomtools/manager.h"
+#include "bayeux/geomtools/geometry_service.h"
+#include "bayeux/geomtools/line_3d.h"
+#include "bayeux/geomtools/helix_3d.h"
+#include "bayeux/geomtools/geomtools.h"
+
+// - Falaise
+#include "falaise/snemo/datamodels/calibrated_data.h"
+#include "falaise/snemo/datamodels/tracker_clustering_data.h"
+#include "falaise/snemo/datamodels/tracker_clustering_solution.h"
+#include "falaise/snemo/datamodels/particle_track_data.h"
+
+// From this application
+
+
+typedef struct TrackEventStorage{
+
+
+
+  std::vector<double> electron_energies_;
+  std::vector<int> electron_charges_;
+  std::vector<int> track_charges_;
+  std::vector<double> electron_track_lengths_;
+  std::vector<int> electron_hit_counts_;
+  std::vector<double> track_curvatures_;
+  int track_count_; // How many reconstructed tracks?
+  int electron_count_; // How many reconstructed electrons?
+
+  
+}trackeventstorage;
+
+
+// This Project
+class TrackCurvature : public dpp::base_module {
+  static const uint minHitsInCluster=3;
+ public:
+  //! Construct module
+  TrackCurvature();
+  //! Destructor
+  virtual ~TrackCurvature();
+  //! Configure the module
+  virtual void initialize(const datatools::properties& myConfig,
+                          datatools::service_manager& flServices,
+                          dpp::module_handle_dict_type& moduleDict);
+  //! Process supplied data record
+  virtual dpp::base_module::process_status process(datatools::things& workItem);
+  //! Reset the module
+  virtual void reset();
+ private:
+  TFile* hfile_;
+  TTree* tree_;
+  TrackEventStorage trackdetails_;
+
+  // configurable data member
+  std::string filename_output_;
+
+  // geometry service
+  const geomtools::manager* geometry_manager_; //!< The geometry manager
+
+  
+  void ResetVars();
+
+  // Macro which automatically creates the interface needed
+  // to enable the module to be loaded at runtime
+  DPP_MODULE_REGISTRATION_INTERFACE(TrackCurvature);
+};
+#endif // TRACKCURVE_HH
+
+
